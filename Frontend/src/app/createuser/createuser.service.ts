35611046511e7cdb9ed3c762cf172a0df0f385f1
Miguel Carvalho <fc54399@alunos.fc.ul.pt>
2022-04-26 00:25:47 +0100

    beans

---
+++ Frontend/src/app/createuser/createuser.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreateuserService {
+  private createuserURL: string = "/createuser";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private router: Router,
+    private http: HttpClient) {
+    //  this.removeUser()
+  }
+
+  createUser(username: string, password: string, role: string): Observable<any>{
+    return this.http.post<any>(`${this.createuserURL}/add`, { username, password, role}, this.httpOptions);
+  }
+}
