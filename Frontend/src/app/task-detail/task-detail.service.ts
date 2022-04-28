93066f7b1c3737434cdcd2399bcbee7467b578c1
brunocotrim1 <bruno.cotrim1@gmail.com>
2022-04-28 19:25:02 +0100

    a

--- Frontend/src/app/task-detail/task-detail.service.ts
+++ Frontend/src/app/task-detail/task-detail.service.ts
@@ -1,15 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 import { User } from '../User';
-
+import { Task } from '../Task';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskDetailService {
 
-  constructor(private http: HttpClient, private authenticationService: AuthenticationService) { } 
-  
+  constructor(private http: HttpClient, private authenticationService: AuthenticationService) { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   getUser(): User {
     const user = this.authenticationService.loadUser();
     if (user)
@@ -18,4 +21,8 @@ export class TaskDetailService {
       return {} as User
   }
 
+
+  getTaskDetail(id: string): Observable<Task> {
+    return this.http.get<any>(`/api/task/${id}`, this.httpOptions);
+  }
 }
