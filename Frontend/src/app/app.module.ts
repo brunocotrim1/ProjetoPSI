c053ab1f17fb42cd4dc51d7bf2ac2ca898be186e
Miguel Carvalho <fc54399@alunos.fc.ul.pt>
2022-04-29 14:21:43 +0100

    links

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { LinkprojectstoteamsComponent } from './linkprojectstoteams/linkprojectstoteams.component';
+
 @NgModule({
   declarations: [
     AppComponent,
